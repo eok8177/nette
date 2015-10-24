af5b5feceba3e76e949723e4af31422246281cd4
Evgen Koromyslov <eok8177@gmail.com>
2015-10-24 15:28:35 +0300

    blog from DB

--- app/FrontModule/presenters/DefaultPresenter.php
+++ app/FrontModule/presenters/DefaultPresenter.php
@@ -9,9 +9,19 @@ use App\Model;
 class DefaultPresenter extends BasePresenter
 {
 
+	/** @var Nette\Database\Context */
+	private $database;
+
+	public function __construct(Nette\Database\Context $database)
+	{
+		$this->database = $database;
+	}
+
 	public function renderDefault()
 	{
-		$this->template->anyVariable = 'any value';
+		$this->template->posts = $this->database->table('posts')
+			->order('created_at DESC')
+			->limit(5);
 	}
 
 }
