9e67732cc30c252d92c0fd6c0e9261474d984a55
Evgen <eok8177@gmail.com>
2015-10-24 16:02:40 +0300

    blog comments

--- app/FrontModule/presenters/PostPresenter.php
+++ app/FrontModule/presenters/PostPresenter.php
@@ -23,5 +23,40 @@ class PostPresenter extends BasePresenter
 		}
 
 		$this->template->post = $post;
+		$this->template->comments = $post->related('comments')->order('created_at');
+	}
+
+	protected function createComponentCommentForm()
+	{
+		$form = new Form;
+
+		$form->addText('name', 'Your name:')
+			->setRequired();
+
+		$form->addText('email', 'Email:');
+
+		$form->addTextArea('content', 'Comment:')
+			->setRequired();
+
+		$form->addSubmit('send', 'Publish comment');
+
+		$form->onSuccess[] = array($this, 'commentFormSucceeded');
+
+		return $form;
+	}
+
+	public function commentFormSucceeded($form, $values)
+	{
+		$postId = $this->getParameter('postId');
+
+		$this->database->table('comments')->insert(array(
+			'post_id' => $postId,
+			'name' => $values->name,
+			'email' => $values->email,
+			'content' => $values->content,
+		));
+
+		$this->flashMessage('Thank you for your comment', 'success');
+		$this->redirect('this');
 	}
 }
