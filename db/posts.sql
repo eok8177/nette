d00ee0b79785b956314ed3264e19a9df82ab0df5
Evgen Koromyslov <eok8177@gmail.com>
2015-10-24 15:28:47 +0300

    SQL

---
+++ db/posts.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) COMMENT='' ENGINE='InnoDB' COLLATE 'utf8_general_ci';
+
+INSERT INTO `posts` (`id`, `title`, `content`, `created_at`) VALUES
+(1, 'Article One',  'Lorem ipusm dolor one',    CURRENT_TIMESTAMP),
+(2, 'Article Two',  'Lorem ipsum dolor two',    CURRENT_TIMESTAMP),
+(3, 'Article Three',    'Lorem ipsum dolor three',  CURRENT_TIMESTAMP);
